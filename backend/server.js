582c212f58fb05f6f55fd4c120a237e3d41041a7
growervpd <henriquemmarins@gmail.com>
2024-12-29 13:41:46 -0300

    Remover opções obsoletas do mongoose.connect

--- backend/server.js
+++ backend/server.js
@@ -13,8 +13,6 @@ const PORT = 3000; // Porta onde o servidor vai rodar
 require('dotenv').config(); // Certifique-se de ter instalado dotenv (npm install dotenv)
 
 mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
 })
   .then(() => console.log('Conectado ao MongoDB!'))
   .catch(err => console.error('Erro ao conectar ao MongoDB:', err));
