7dba7a59741926ab2f80fcbcb20be69378db2566
growervpd <henriquemmarins@gmail.com>
2025-01-03 16:07:30 -0300

    Adicionando liga e desliga do AC

--- backend/temperature-checker.js
+++ backend/temperature-checker.js
@@ -2,43 +2,46 @@ const axios = require("axios");
 const crypto = require("crypto");
 
 // Configurações da API Tuya
-const ClientID = "sjsmr9rtnsn8fgj7rrce"; // Substitua pelo seu ClientID
-const ClientSecret = "9bb34ec30170490eb03dd45532f1bf53"; // Substitua pelo seu ClientSecret
-const BaseUrl = "https://openapi.tuyaus.com"; // URL base da Tuya Cloud
-const DeviceId = "ebf025fcebde746b5akmak"; // ID do dispositivo Tuya
 const debug = true;
+const ClientID = "sjsmr9rtnsn8fgj7rrce";
+const ClientSecret = "9bb34ec30170490eb03dd45532f1bf53";
+const BaseUrl = "https://openapi.tuyaus.com";
+const EmptyBodyEncoded = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
 
 // Função para gerar a assinatura HMAC-SHA256
 function generateSignature(stringToSign, secret) {
   return crypto
-    .createHmac("sha256", secret)
+    .createHmac('sha256', secret)
     .update(stringToSign)
-    .digest("hex")
+    .digest('hex')
     .toUpperCase();
 }
 
-// Função para obter o token de acesso
+// Obter token de acesso
 async function getAccessToken() {
-  const tuyatime = `${Date.now()}`;
+  const tuyatime = `${Date.now()}`; // Gera um timestamp a cada requisição
   const URL = "/v1.0/token?grant_type=1";
-  const StringToSign = `${ClientID}${tuyatime}GET\n\n${URL}`;
+  const StringToSign = `${ClientID}${tuyatime}GET\n${EmptyBodyEncoded}\n\n${URL}`;
+  if (debug) console.log(`StringToSign is now: ${StringToSign}`);
+
   const AccessTokenSign = generateSignature(StringToSign, ClientSecret);
+  if (debug) console.log(`AccessTokenSign is now: ${AccessTokenSign}`);
 
   try {
     const response = await axios.get(`${BaseUrl}${URL}`, {
       headers: {
-        "sign_method": "HMAC-SHA256",
-        "client_id": ClientID,
-        t: tuyatime,
-        mode: "cors",
-        "Content-Type": "application/json",
-        sign: AccessTokenSign,
+        'sign_method': 'HMAC-SHA256',
+        'client_id': ClientID,
+        't': tuyatime,
+        'mode': 'cors',
+        'Content-Type': 'application/json',
+        'sign': AccessTokenSign,
       },
     });
-    if (debug) console.log("AccessToken Response:", response.data);
+    if (debug) console.log(`AccessTokenResponse is now:`, response.data);
     return response.data.result.access_token;
   } catch (error) {
-    console.error("Erro ao obter o token de acesso:", error.message);
+    console.error("Error fetching Access Token:", error.message);
     throw error;
   }
 }
