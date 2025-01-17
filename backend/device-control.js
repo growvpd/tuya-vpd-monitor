c36384ee6c09bf21417bda7241e58078269d7cff
growervpd <henriquemmarins@gmail.com>
2025-01-17 14:00:41 -0300

    pagina para ligar e desligar dispositivos

--- backend/device-control.js
+++ backend/device-control.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const axios = require("axios");
-const { ClientID, ClientSecret, getAccessToken, generateSignature, BaseUrl } = require('./tuya'); // Importar funções e variáveis do arquivo `tuya.js`
+const { ClientID, ClientSecret, generateSignature, BaseUrl } = require('./tuya'); // Importar funções e variáveis do arquivo `tuya.js`
 
 
 const router = express.Router();
