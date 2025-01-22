997f16f6558006ac7e2be654ab9816543fc81da2
growervpd <henriquemmarins@gmail.com>
2025-01-22 00:47:10 -0300

    Rev3 - Frontend do controle de dispositivo.

--- backend/device-control.js
+++ backend/device-control.js
@@ -1,190 +1,31 @@
 const express = require("express");
-const axios = require("axios");
-const crypto = require("crypto");
-const { ClientID, ClientSecret, generateSignature, EmptyBodyEncoded, debug, getTemperature, updateTemperature , getDeviceStatus , extractTemperatureAndHumidity , fetchTuyaDataWithCache } = require("./tuya");
+const { getDeviceStatus, thermostatDeviceId, switchDeviceId } = require("./tuya");
 
 const router = express.Router();
-const deviceId = "ebf025fcebde746b5akmak"; // ID do dispositivo
-const BaseUrl = "https://openapi.tuyaus.com"; // URL base da API Tuya
 
-// Configurações de temperatura
-let minTemperature = 24; // Temperatura mínima para desligar o AC
-let maxTemperature = 26; // Temperatura máxima para ligar o AC
-
-// Cache para o token de acesso
-let cachedToken = null;
-let tokenExpiration = null;
-
-/**
- * Obter token de acesso com cache
- */
-async function getAccessToken() {
-  const currentTime = Date.now();
-
-  if (cachedToken && tokenExpiration && currentTime < tokenExpiration) {
-    console.log("Usando token do cache:", cachedToken);
-    return cachedToken;
-  }
-
-  const tuyatime = Math.floor(currentTime).toString();
-  const URL = "/v1.0/token?grant_type=1";
-  const StringToSign = `${ClientID}${tuyatime}GET\n${EmptyBodyEncoded}\n\n${URL}`;
-  if (debug) console.log("StringToSign is now:", StringToSign);
-
-  const AccessTokenSign = generateSignature(StringToSign, ClientSecret);
-  if (debug) console.log("AccessTokenSign is now:", AccessTokenSign);
-
+// Rota para obter status do termostato
+router.get("/status/thermostat", async (req, res) => {
   try {
-    const response = await axios.get(`${BaseUrl}${URL}`, {
-      headers: {
-        sign_method: "HMAC-SHA256",
-        client_id: ClientID,
-        t: tuyatime,
-        "Content-Type": "application/json",
-        sign: AccessTokenSign,
-      },
-    });
+    const status = await getDeviceStatus(thermostatDeviceId);
+    const temperature = status.find((item) => item.code === "va_temperature")?.value / 10 || null;
+    const humidity = status.find((item) => item.code === "va_humidity")?.value / 10 || null;
 
-    if (response.data.success) {
-      cachedToken = response.data.result.access_token;
-      tokenExpiration = currentTime + response.data.result.expire_time * 1000;
-      console.log("Novo token gerado:", cachedToken);
-      return cachedToken;
-    } else {
-      throw new Error(`Erro ao obter token: ${response.data.msg}`);
-    }
+    res.json({ success: true, temperature, humidity });
   } catch (error) {
-    console.error("Erro ao buscar Access Token:", error.message);
-    throw error;
-  }
-}
-
-/**
- * Enviar comando ao dispositivo
- */
-async function sendDeviceCommand(commandCode, commandValue) {
-  try {
-    const accessToken = await getAccessToken();
-    const tuyatime = Math.floor(Date.now()).toString();
-    const URL = `/v1.0/iot-03/devices/${deviceId}/commands`;
-    const body = JSON.stringify({
-      commands: [{ code: commandCode, value: commandValue }],
-    });
-    const Content_SHA256 = crypto.createHash("sha256").update(body, "utf8").digest("hex");
-    const StringToSign = `${ClientID}${accessToken}${tuyatime}POST\n${Content_SHA256}\n\n${URL}`;
-    const RequestSign = generateSignature(StringToSign, ClientSecret);
-
-    const response = await axios.post(
-      `${BaseUrl}${URL}`,
-      { commands: [{ code: commandCode, value: commandValue }] },
-      {
-        headers: {
-          sign_method: "HMAC-SHA256",
-          client_id: ClientID,
-          t: tuyatime,
-          "Content-Type": "application/json",
-          sign: RequestSign,
-          access_token: accessToken,
-        },
-      }
-    );
-    console.log("Comando enviado com sucesso:", response.data);
-    return response.data;
-  } catch (error) {
-    console.error("Erro ao enviar comando para o dispositivo:", error.message);
-    throw error;
-  }
-}
-
-/**
- * Monitorar e controlar automaticamente o AC com base na temperatura
- */
-async function monitorTemperature() {
-  try {
-    const deviceStatus = await fetchTuyaDataWithCache(deviceId);
-    const sensorData = extractTemperatureAndHumidity(deviceStatus);
-
-    if (!sensorData) {
-      console.log("Dispositivo sem sensores de temperatura/umidade. Nenhuma ação necessária.");
-      return; // Sai da função se não houver sensores
-    }
-
-    const { temperature } = sensorData;
-    console.log("Temperatura atual:", temperature);
-    console.log("Temperatura mínima:", minTemperature);
-    console.log("Temperatura máxima:", maxTemperature);
-
-    if (temperature >= maxTemperature) {
-      console.log(`Temperatura ${temperature} acima de ${maxTemperature}. Ligando o ar-condicionado...`);
-      await sendDeviceCommand("switch_1", true);
-    } else if (temperature <= minTemperature) {
-      console.log(`Temperatura ${temperature} abaixo de ${minTemperature}. Desligando o ar-condicionado...`);
-      await sendDeviceCommand("switch_1", false);
-    } else {
-      console.log(`Temperatura ${temperature} está dentro da faixa aceitável. Nenhuma ação necessária.`);
-    }
-  } catch (error) {
-    console.error("Erro ao monitorar temperatura:", error.message);
-  }
-}
-
-
-// Configuração para monitoramento automático a cada 30 segundos
-setInterval(monitorTemperature,  1 * 60 * 1000);
-
-// Rotas
-router.post("/on", async (req, res) => {
-  try {
-    const result = await sendDeviceCommand("switch_1", true);
-    res.json({ success: true, message: "Dispositivo ligado com sucesso!", data: result });
-  } catch (error) {
-    res.status(500).json({ success: false, message: "Erro ao ligar o dispositivo.", error: error.message });
+    res.status(500).json({ success: false, message: "Erro ao obter dados do termostato.", error: error.message });
   }
 });
 
-router.post("/off", async (req, res) => {
+// Rota para obter status do interruptor
+router.get("/status/switch", async (req, res) => {
   try {
-    const result = await sendDeviceCommand("switch_1", false);
-    res.json({ success: true, message: "Dispositivo desligado com sucesso!", data: result });
-  } catch (error) {
-    res.status(500).json({ success: false, message: "Erro ao desligar o dispositivo.", error: error.message });
-  }
-});
+    const status = await getDeviceStatus(switchDeviceId);
+    const switchStatus = status.find((item) => item.code === "switch_1")?.value || false;
 
-router.post("/set-temperature", (req, res) => {
-  const { minTemp, maxTemp } = req.body;
-  if (minTemp !== undefined) minTemperature = parseFloat(minTemp);
-  if (maxTemp !== undefined) maxTemperature = parseFloat(maxTemp);
-  res.json({ success: true, message: "Temperaturas atualizadas com sucesso!", minTemperature, maxTemperature });
+    res.json({ success: true, switchStatus });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Erro ao obter dados do interruptor.", error: error.message });
+  }
 });
 
 module.exports = router;
-
-// Rota para obter o estado atual do dispositivo
-router.get("/status", async (req, res) => {
-  try {
-    // Obtem o token de acesso
-    const accessToken = await getAccessToken();
-
-    // Obtém o status do dispositivo
-    const deviceStatus = await getDeviceStatus(accessToken, deviceId);
-
-    // Extrai informações relevantes
-    const { temperature, humidity } = extractTemperatureAndHumidity(deviceStatus);
-
-    const powerStatus = deviceStatus.find(device => device.id === deviceId)?.status.find(s => s.code === "switch_1")?.value;
-
-    // Retorna as informações no formato JSON
-    res.json({
-      success: true,
-      temperature,
-      humidity,
-      powerStatus: powerStatus ? "Ligado" : "Desligado"
-    });
-  
-  } catch (error) {
-    console.error("Erro ao obter o estado do dispositivo:", error.message);
-    res.status(500).json({ success: false, message: "Erro ao obter o estado do dispositivo.", error: error.message });
-  }
-});
-
--- backend/tuya.js
+++ backend/tuya.js
@@ -1,196 +1,35 @@
 const axios = require('axios');
 const crypto = require('crypto');
 
-// Configurações
+// Configurações gerais
 const debug = true;
-const ClientID = "7j3tg7crd8gxr4rdsu7s";
-const ClientSecret = "ed01098ca59c40d2845de5ef25bb1dc9";
+const ClientID = "SEU_CLIENT_ID";
+const ClientSecret = "SEU_CLIENT_SECRET";
 const BaseUrl = "https://openapi.tuyaus.com";
-const EmptyBodyEncoded = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
-const deviceIds = "eb8faf00e42469ffaahezh";
+const EmptyBodyEncoded = crypto.createHash("sha256").update("", "utf8").digest("hex");
 
-// Função para gerar a assinatura HMAC-SHA256
+// IDs dos dispositivos
+const thermostatDeviceId = "ID_TERMOSTATO"; // Substituir pelo ID do termostato
+const switchDeviceId = "ID_INTERRUPTOR";   // Substituir pelo ID do interruptor
+
+// Função para gerar assinatura HMAC-SHA256
 function generateSignature(stringToSign, secret) {
-  return crypto
-    .createHmac('sha256', secret)
-    .update(stringToSign)
-    .digest('hex')
-    .toUpperCase();
+  return crypto.createHmac('sha256', secret).update(stringToSign).digest('hex').toUpperCase();
 }
 
-// Obter token de acesso
+// Função para obter token de acesso
+let cachedToken = null;
+let tokenExpiration = null;
+
 async function getAccessToken() {
-  const tuyatime = `${Date.now()}`; // Gera um timestamp a cada requisição
-  const URL = "/v1.0/token?grant_type=1";
-  const StringToSign = `${ClientID}${tuyatime}GET\n${EmptyBodyEncoded}\n\n${URL}`;
-  if (debug) console.log(`StringToSign is now: ${StringToSign}`);
+  const currentTime = Date.now();
 
-  const AccessTokenSign = generateSignature(StringToSign, ClientSecret);
-  if (debug) console.log(`AccessTokenSign is now: ${AccessTokenSign}`);
-
-  try {''
-    const response = await axios.get(`${BaseUrl}${URL}`, {
-      headers: {
-        'sign_method': 'HMAC-SHA256',
-        'client_id': ClientID,
-        't': tuyatime,
-        'mode': 'cors',
-        'Content-Type': 'application/json',
-        'sign': AccessTokenSign,
-      },
-    });
-    if (debug) console.log(`AccessTokenResponse is now:`, response.data);
-    return response.data.result.access_token;
-  } catch (error) {
-    console.error("Error fetching Access Token:", error.message);
-    throw error;
-  }
-}
-
-// Obter status do dispositivo
-async function getDeviceStatus(accessToken, deviceIds) {
-  const tuyatime = `${Date.now()}`; // Gera um timestamp a cada requisição
-  const URL = `/v1.0/iot-03/devices/status?device_ids=${deviceIds}`;
-  const StringToSign = `${ClientID}${accessToken}${tuyatime}GET\n${EmptyBodyEncoded}\n\n${URL}`;
-  if (debug) console.log(`StringToSign is now: ${StringToSign}`);
-
-  const RequestSign = generateSignature(StringToSign, ClientSecret);
-  if (debug) console.log(`RequestSign is now: ${RequestSign}`);
-
-  try {
-    const response = await axios.get(`${BaseUrl}${URL}`, {
-      headers: {
-        'sign_method': 'HMAC-SHA256',
-        'client_id': ClientID,
-        't': tuyatime,
-        'mode': 'cors',
-        'Content-Type': 'application/json',
-        'sign': RequestSign,
-        'access_token': accessToken,
-      },
-    });
-    if (debug) console.log(`RequestResponse is now:`, response.data);
-    return response.data.result;
-  } catch (error) {
-    console.error("Error fetching Device Status:", error.message);
-    throw error;
-  }
-}
-
-// Função para obter os dados do dispositivo (token + status)
-let cachedData = null;
-let cacheTimestamp = null;
-const CACHE_DURATION = 5 * 60 * 1000; // Cache válido por 5 minutos
-
-async function fetchTuyaDataWithCache(deviceIds) {
-  if (cachedData && cacheTimestamp && Date.now() - cacheTimestamp < CACHE_DURATION) {
-    console.log("Retornando dados do cache.");
-    return cachedData;
+  if (cachedToken && tokenExpiration && currentTime < tokenExpiration) {
+    console.log("Usando token do cache:", cachedToken);
+    return cachedToken;
   }
 
-  const accessToken = await getAccessToken();
-  const deviceStatus = await getDeviceStatus(accessToken, deviceIds);
-
-  cachedData = deviceStatus; // Atualiza o cache
-  cacheTimestamp = Date.now(); // Atualiza o timestamp
-  console.log("Dados atualizados no cache.");
-  return cachedData;
-}
-
-// Variavel para checar temperatura no ar
-let checkTemperature = null;
-
-// Função para extrair temperatura e umidade do status do dispositivo
-function extractTemperatureAndHumidity(deviceStatus) {
-  let temperature = null;
-  let humidity = null;
-
-  deviceStatus.forEach((device) => {
-    device.status.forEach((item) => {
-      if (item.code === 'va_temperature') {
-        temperature = item.value / 10; // Normalizar para °C, se necessário
-      } else if (item.code === 'va_humidity') {
-        humidity = item.value / 10;
-      }
-    });
-  });
-
-  console.log("Temperatura:", JSON.stringify(temperature, null, 2));
-  console.log("Humidade:", JSON.stringify(humidity, null, 2));
-
-  if (temperature === null || humidity === null) {
-    throw new Error('Não foi possível encontrar temperatura ou umidade no status do dispositivo.');
-  }
-
-  return { temperature, humidity };
-
-}
-
-// Função para obter e atualizar a temperatura atual do dispositivo
-async function updateTemperature() {
-  try {
-    // Obtém o status do dispositivo utilizando a função com cache para evitar chamadas redundantes
-    const deviceStatus = await fetchTuyaDataWithCache(deviceIds);
-
-    // Extrai a temperatura e a umidade do status do dispositivo retornado
-    const { temperature } = extractTemperatureAndHumidity(deviceStatus);
-
-    // Atualiza a variável global `checkTemperature` com a temperatura obtida
-    checkTemperature = temperature;
-
-    // Loga a temperatura atualizada no console para fins de depuração
-    console.log("Temperatura check atualizada:", checkTemperature);
-  } catch (error) {
-    // Em caso de erro, exibe uma mensagem de erro no console com detalhes
-    console.error("Erro ao atualizar temperatura check:", error.message);
-  }
-}
-
-// Exportações
-module.exports = {
-  getAccessToken,
-  getDeviceStatus,
-  fetchTuyaDataWithCache,
-  extractTemperatureAndHumidity,
-  deviceIds,
-  ClientID,
-  ClientSecret,
-  getAccessToken,
-  generateSignature,
-  BaseUrl,
-  EmptyBodyEncoded,
-  debug,
-  getTemperature: () => checkTemperature,
-  updateTemperature,
-};
-  
-// Apenas para testes manuais ou depuração, não necessário na execução contínua
-if (debug) {
-  (async function () {
-    try {
-      const deviceStatus = await fetchTuyaDataWithCache(deviceIds);
-      console.log("Device Status:", deviceStatus);
-
-      const { temperature, humidity } = extractTemperatureAndHumidity(deviceStatus);
-      console.log("Temperatura:", temperature, "Humidade:", humidity, "Check Temperature:", checkTemperature);
-    } catch (error) {
-      console.error("An error occurred:", error.message);
-    }
-  })(); 
-}
-
-// Exporte a função no arquivo tuya.js
-function generateSignature(stringToSign, secret) {
-  return crypto
-    .createHmac('sha256', secret)
-    .update(stringToSign)
-    .digest('hex')
-    .toUpperCase();
-}
-
-// Adicione a função getAccessToken ao module.exports no tuya.js
-async function getAccessToken() {
-  const tuyatime = `${Date.now()}`;
+  const tuyatime = `${currentTime}`;
   const URL = "/v1.0/token?grant_type=1";
   const StringToSign = `${ClientID}${tuyatime}GET\n${EmptyBodyEncoded}\n\n${URL}`;
   const AccessTokenSign = generateSignature(StringToSign, ClientSecret);
@@ -201,15 +40,53 @@ async function getAccessToken() {
         sign_method: "HMAC-SHA256",
         client_id: ClientID,
         t: tuyatime,
-        mode: "cors",
-        "Content-Type": "application/json",
         sign: AccessTokenSign,
       },
     });
-    return response.data.result.access_token;
+
+    if (response.data.success) {
+      cachedToken = response.data.result.access_token;
+      tokenExpiration = currentTime + response.data.result.expire_time * 1000;
+      console.log("Novo token gerado:", cachedToken);
+      return cachedToken;
+    } else {
+      throw new Error(`Erro ao obter token: ${response.data.msg}`);
+    }
   } catch (error) {
-    console.error("Error fetching Access Token:", error.message);
+    console.error("Erro ao buscar token de acesso:", error.message);
     throw error;
   }
 }
 
+// Função para buscar status de um dispositivo
+async function getDeviceStatus(deviceId) {
+  const accessToken = await getAccessToken();
+  const tuyatime = `${Date.now()}`;
+  const URL = `/v1.0/iot-03/devices/${deviceId}/status`;
+  const StringToSign = `${ClientID}${accessToken}${tuyatime}GET\n${EmptyBodyEncoded}\n\n${URL}`;
+  const RequestSign = generateSignature(StringToSign, ClientSecret);
+
+  try {
+    const response = await axios.get(`${BaseUrl}${URL}`, {
+      headers: {
+        sign_method: "HMAC-SHA256",
+        client_id: ClientID,
+        t: tuyatime,
+        sign: RequestSign,
+        access_token: accessToken,
+      },
+    });
+    return response.data.result;
+  } catch (error) {
+    console.error(`Erro ao buscar status do dispositivo ${deviceId}:`, error.message);
+    throw error;
+  }
+}
+
+// Exportações
+module.exports = {
+  getAccessToken,
+  getDeviceStatus,
+  thermostatDeviceId,
+  switchDeviceId,
+};
